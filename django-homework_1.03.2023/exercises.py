a9fc7a175f4e7ee82afbfe90a41dad3ed8507b4e
DimahSSS <112540898+Dimash357@users.noreply.github.com>
2023-03-10 23:56:08 +0600

    Add files via upload

---
+++ django-homework_1.03.2023/exercises.py
@@ -0,0 +1,30 @@
+# Task 1
+a = int(input("Сторона a: "))
+b = int(input("Сторона b: "))
+c = int(input("Сторона c: "))
+if (a + b > c) and (a + c > b) and (b + c > a):
+    print("true")
+else:
+    print("false")
+
+# Task 2
+a = int(input("Число a: "))
+if a % 2 == 0:
+    print("true")
+
+# Task 3
+a = int(input("Число a: "))
+b = int(input("Число b: "))
+c = int(input("Число c: "))
+if a + b > c:
+    print("true")
+else:
+    print("false")
+
+# Task 4
+a = int(input("Число a: "))
+b = int(input("Число b: "))
+if a > b:
+    print("True")
+else:
+    print("False")
